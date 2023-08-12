fa44b7052489e4cecb95aad8bdfd90218b0d8c45
atlmapper <ngalford@gmail.com>
2023-08-12 13:14:48 -0400

    added simple logging

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from "express";
 import getAllInnerPages from "./crawler.js";
 import { setEmbeddings } from "./setEmbeddings.js";
 import getOpenAIStream from "./docs.js";
-import expressBasicAuth from 'express-basic-auth'
+import * as basicAuth from 'express-basic-auth'
 
 
 const app = express();
 
 app.use(express.json());
-app.use(expressBasicAuth({
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+app.use(basicAuth({
   users: { 'admin': process.env.DOCIT_AUTH }
 }))
 const port = process.env.PORT || 3000;
@@ -29,6 +31,7 @@ app.post('/PageEmbeddings', (req: Request, res: Response) => {
 })
 
 app.get('/questions', async (req: Request, res: Response) => {
+  console.log('user: ', req.headers.authorization);
   const answer = await getOpenAIStream(req.body.question, req.body.projectID);
   res.set('Content-Type', 'application/json').status(200).send({ answer })
 });
